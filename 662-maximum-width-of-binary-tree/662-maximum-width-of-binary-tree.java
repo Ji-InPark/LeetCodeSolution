c16a67e59bb9d8436bc93ecbd91615a38af9dacd
Jin Park <81283634+Ji-InPark@users.noreply.github.com>
2022-02-27 22:27:05 +0900

    Time: 1 ms (98.94%), Space: 41.5 MB (51.38%) - LeetHub

---
+++ 662-maximum-width-of-binary-tree/662-maximum-width-of-binary-tree.java
@@ -0,0 +1,67 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    int result = 0;
+    public int widthOfBinaryTree(TreeNode root) {
+        Queue<TreeNode> q = new LinkedList<TreeNode>();
+        
+        root.val = 1;
+        q.add(root);
+        
+        bfs(q);
+
+        
+        return result;
+    }
+    
+    public void bfs(Queue<TreeNode> q)
+    {
+        if(q.isEmpty())
+            return;
+        
+        Queue<TreeNode> next = new LinkedList<TreeNode>();
+        
+        int first = 0, last = 0;
+        
+        for(int i = 0; !q.isEmpty(); i += 2)
+        {
+            TreeNode temp = q.poll();
+            if(temp == null) continue;
+            
+            if(temp.left != null)
+            {
+                temp.left.val = temp.val * 2;
+                if(first == 0)
+                    first = temp.left.val;
+                last = temp.left.val;
+            }
+            if(temp.right != null)
+            {   
+                temp.right.val = temp.val * 2 + 1;
+                if(first == 0)
+                    first = temp.right.val;
+                last = temp.right.val;
+            }
+            
+            result = Math.max(result, last - first + 1);
+
+            next.add(temp.left);
+            next.add(temp.right);
+        }
+        
+        bfs(next);
+    }
+}
