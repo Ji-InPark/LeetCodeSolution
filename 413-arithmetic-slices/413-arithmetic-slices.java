a549406f402d656383e897952d2fa18cb6e2b7f6
Jin Park <81283634+Ji-InPark@users.noreply.github.com>
2022-03-03 11:03:09 +0900

    Time: 17 ms (5.43%), Space: 41.6 MB (37.06%) - LeetHub

---
+++ 413-arithmetic-slices/413-arithmetic-slices.java
@@ -0,0 +1,48 @@
+class Solution {
+    public int numberOfArithmeticSlices(int[] nums) {
+        if(nums.length < 3)
+            return 0;
+        
+        int result = 0;
+        int first = 0, last = -1;
+        int diff = nums[0] - nums[1];
+        
+        for(int i = 1; i < nums.length - 1; i++)
+        {
+            if(nums[i] - nums[i + 1] != diff)
+            {
+                if(last - first + 1 >= 3)
+                {
+                    System.out.println("i: " + i + " | first: " + first + " | last: " + last);
+                    result += calc(last - first + 1);
+                    System.out.println("result: " + result);
+                }
+                    
+                first = i;
+                last = i + 1;
+                diff = nums[i] - nums[i + 1];
+            }
+            else
+            {
+                last = i + 1;
+            }
+        }
+        
+        if(last - first + 1 >= 3)
+        {
+            result += calc(last - first + 1);
+        }
+        
+        return result;
+    }
+    
+    public int calc(int num)
+    {
+        int result = 0;
+        for(int i = 3; i <= num; i++)
+        {
+            result += num + 1 - i;
+        }
+        return result;
+    }
+}
