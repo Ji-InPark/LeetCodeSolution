dab90e569046cbecd5cea8385de1192a1ad8a078
Jin Park <81283634+Ji-InPark@users.noreply.github.com>
2022-03-06 10:57:02 +0900

    Time: 0 ms (100.00%), Space: 41.9 MB (22.90%) - LeetHub

--- 1359-count-all-valid-pickup-and-delivery-options/1359-count-all-valid-pickup-and-delivery-options.java
+++ 1359-count-all-valid-pickup-and-delivery-options/1359-count-all-valid-pickup-and-delivery-options.java
@@ -1,15 +1,12 @@
 class Solution {
     public int countOrders(int n) {
+        int mod = 1000000007;
         long result = 1;
-        int diff = 9, mul = 6;
-        n--;
-        while(n > 0)
+        for(int i = 2; i <= n; i++)
         {
-            result *= mul;
-            mul += diff;
-            diff += 4;
-            result %= 1000000007;
-            n--;
+            int space = (i - 1) * 2 + 1;
+            result = result * (space * (space + 1) / 2);
+            result %= mod;
         }
         
         return (int)result;
