ecfb84612ffa8b7628de692fd24a94d14f9bb4b5
Jin Park <81283634+Ji-InPark@users.noreply.github.com>
2022-03-10 20:47:53 +0900

    Time: 3 ms (73.45%), Space: 47.9 MB (27.97%) - LeetHub

--- 2-add-two-numbers/2-add-two-numbers.java
+++ 2-add-two-numbers/2-add-two-numbers.java
@@ -1,32 +1,20 @@
 import java.math.BigInteger;
 
 class Solution {
-    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        Queue<Integer> s1 = new LinkedList<Integer>();
-        Queue<Integer> s2 = new LinkedList<Integer>();
-        
-        for(int i = 0; l1 != null; i++) 
-        {
-            s1.add(l1.val);
-            l1 = l1.next;
-        }
-        
-        for(int i = 0; l2 != null; i++)
-        {
-            s2.add(l2.val);
-            l2 = l2.next;
-        }
-        
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) { 
         ListNode result = new ListNode();
         ListNode node = result;
         int next = 0;
         
-        while(!s1.isEmpty() || !s2.isEmpty())
+        while(l1 != null || l2 != null)
         {
-            int now = next + (s1.isEmpty() ? 0 : s1.poll()) + (s2.isEmpty() ? 0 : s2.poll());
+            int now = next + (l1 == null ? 0 : l1.val) + (l2 == null ? 0 : l2.val);
             node.next = new ListNode(now % 10);
             next = now / 10;
             node = node.next;
+            
+            if(l1 != null) l1 = l1.next;
+            if(l2 != null) l2 = l2.next;
         }
         
         if(next > 0)
