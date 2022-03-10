efd219dc86858e05b4ffd03c93b05b2c5b694a66
Jin Park <81283634+Ji-InPark@users.noreply.github.com>
2022-03-10 19:51:27 +0900

    Time: 6 ms (9.67%), Space: 48.7 MB (5.43%) - LeetHub

--- 2-add-two-numbers/2-add-two-numbers.java
+++ 2-add-two-numbers/2-add-two-numbers.java
@@ -1,57 +1,40 @@
-/**
- * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
- * }
- */
 import java.math.BigInteger;
 
 class Solution {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        BigInteger first = new BigInteger("0"), second = new BigInteger("0"), result = new BigInteger("0");
-        BigInteger f_index = new BigInteger("1"), s_index = new BigInteger("1");
+        Queue<Integer> s1 = new LinkedList<Integer>();
+        Queue<Integer> s2 = new LinkedList<Integer>();
         
-        while(l1.next != null)
+        for(int i = 0; l1 != null; i++) 
         {
-            first = first.add(f_index.multiply(BigInteger.valueOf(l1.val)));
-            f_index = f_index.multiply(BigInteger.valueOf(10));
+            s1.add(l1.val);
             l1 = l1.next;
         }
-        first = first.add(f_index.multiply(BigInteger.valueOf(l1.val)));
         
-        while(l2.next != null)
+        for(int i = 0; l2 != null; i++)
         {
-            second = second.add(s_index.multiply(BigInteger.valueOf(l2.val)));
-            s_index = s_index.multiply(BigInteger.valueOf(10));
+            s2.add(l2.val);
             l2 = l2.next;
         }
-        second = second.add(s_index.multiply(BigInteger.valueOf(l2.val)));
         
-        result = result.add(first);
-        result = result.add(second);
+        ListNode result = new ListNode();
+        ListNode node = result;
+        int next = 0;
         
-        ListNode rNode = new ListNode();
-        ListNode tail = rNode;
-        
-        while(true)
+        while(!s1.isEmpty() || !s2.isEmpty())
         {
-            ListNode temp = new ListNode();
-            
-            tail.val = result.remainder(BigInteger.valueOf(10)).intValue();
-            result = result.divide(BigInteger.valueOf(10));
-            
-            if(result.toString().equals("0"))
-                break;
-            
-            tail.next = temp;
-            tail = temp;
+            int now = next + (s1.isEmpty() ? 0 : s1.poll()) + (s2.isEmpty() ? 0 : s2.poll());
+            node.next = new ListNode(now % 10);
+            next = now / 10;
+            node = node.next;
         }
         
-        return rNode;
-
+        if(next > 0)
+        {
+            node.next = new ListNode(next);
+            node = node.next;
+        }
+        
+        return result.next;
     }
 }
