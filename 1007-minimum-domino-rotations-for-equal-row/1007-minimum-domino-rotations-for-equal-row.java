60364bc9f8c70fa0d17a1acac16fe9484ad82fb6
Jin Park <81283634+Ji-InPark@users.noreply.github.com>
2022-03-20 23:21:11 +0900

    Time: 6 ms (58.07%), Space: 94.7 MB (26.27%) - LeetHub

--- 1007-minimum-domino-rotations-for-equal-row/1007-minimum-domino-rotations-for-equal-row.java
+++ 1007-minimum-domino-rotations-for-equal-row/1007-minimum-domino-rotations-for-equal-row.java
@@ -3,36 +3,30 @@ class Solution {
         int[] t = new int[7];
         int[] b = new int[7];
         
-        ArrayList<Integer> arr = new ArrayList<>();
-        
         for(int i = 0; i < tops.length; i++)
         {
             t[tops[i]]++;
             b[bottoms[i]]++;
         }
         
-        for(int i = 1; i < t.length; i++)
-        {
-            if(t[i] + b[i] >= tops.length)
-                arr.add(i);
-        }
-        
         int result = 20005;
         
         Loop:
-        for(int i = 0; i < arr.size(); i++)
+        for(int i = 1; i < t.length; i++)
         {
-            int now = arr.get(i);
-            int tnum = 0, bnum = 0;
-            for(int j = 0; j < tops.length; j++)
+            if(t[i] + b[i] >= tops.length)
             {
-                if(tops[j] == now && bottoms[j] == now) continue;
-                else if(tops[j] == now && bottoms[j] != now) tnum++;
-                else if(tops[j] != now && bottoms[j] == now) bnum++;
-                else continue Loop;   
+                int tnum = 0, bnum = 0;
+                for(int j = 0; j < tops.length; j++)
+                {
+                    if(tops[j] == i && bottoms[j] == i) continue;
+                    else if(tops[j] == i && bottoms[j] != i) tnum++;
+                    else if(tops[j] != i && bottoms[j] == i) bnum++;
+                    else continue Loop;   
+                }
+                
+                result = Math.min(result, Math.min(tnum, bnum));
             }
-            
-            result = Math.min(result, Math.min(tnum, bnum));
         }
         
         if(result == 20005)
