51637c88d6664babbb1209b273de80efd0953c28
Jin Park <81283634+Ji-InPark@users.noreply.github.com>
2022-03-24 20:53:17 +0900

    Time: 5 ms (99.93%), Space: 68.6 MB (5.39%) - LeetHub

--- 881-boats-to-save-people/881-boats-to-save-people.java
+++ 881-boats-to-save-people/881-boats-to-save-people.java
@@ -1,12 +1,26 @@
 class Solution {
     public int numRescueBoats(int[] people, int limit) {
-        Arrays.sort(people);
+        int[] arr = new int[limit + 1];
         int result = 0;
         int left = 0, right = people.length - 1;
         
-        while(right > left)
+        for(int num : people)
+            arr[num]++;
+        
+        int index = 1;
+        
+        for(int i = 0; i < people.length; i++)
         {
-            if(people[right--] + people[left] <= limit) left++;
+            while(arr[index] == 0) index++;
+            
+            arr[index]--;
+            
+            people[i] = index;
+        }
+        
+        while(left < right)
+        {
+             if(people[right--] + people[left] <= limit) left++;
             
             result++;
         }
