ee2ec5e37e821b3fc77eb3835ab439007fb2c655
Jin Park <81283634+Ji-InPark@users.noreply.github.com>
2022-03-26 11:10:27 +0900

    Time: 0 ms (100.00%), Space: 54.3 MB (51.69%) - LeetHub

---
+++ 704-binary-search/704-binary-search.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int search(int[] nums, int target) {
+        int left = 0, right = nums.length - 1, middle;
+        
+        while(left <= right)
+        {
+            middle = (left + right) / 2;
+            
+            if(nums[middle] == target)
+            {
+                return middle;
+            }
+            else if(nums[middle] > target)
+            {
+                right = middle - 1;
+            }
+            else
+            {
+                left = middle + 1;
+            }
+        }
+        
+        return -1;
+    }
+}
