c7c445fe35ad9255df01deff5f83a0365c01f4a3
Jin Park <81283634+Ji-InPark@users.noreply.github.com>
2022-04-23 10:31:37 +0900

    Time: 5 ms (58.12%), Space: 43.8 MB (30.07%) - LeetHub

---
+++ 535-encode-and-decode-tinyurl/535-encode-and-decode-tinyurl.java
@@ -0,0 +1,21 @@
+public class Codec {
+    HashMap<Integer, String> encodeMap = new HashMap<>();
+    HashMap<String, Integer> decodeMap = new HashMap<>();
+    int index = 0;
+
+    public String encode(String longUrl) {
+        if(!decodeMap.containsKey(longUrl)) 
+        {
+            encodeMap.put(index, longUrl);
+            decodeMap.put(longUrl, index++);
+        }
+
+        return "http://tinyurl.com/" + decodeMap.get(longUrl);
+    }
+
+    public String decode(String shortUrl) {
+        int input = Integer.parseInt(shortUrl.substring(19));
+        
+        return encodeMap.get(input);
+    }
+}
