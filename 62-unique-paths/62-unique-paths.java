3e650c0de28b43072047d08ce3822e64945da88f
Ji-In Park <81283634+Ji-InPark@users.noreply.github.com>
2022-08-01 19:50:19 +0900

    Time: 1 ms (55.39%), Space: 40.9 MB (48.79%) - LeetHub

--- 62-unique-paths/62-unique-paths.java
+++ 62-unique-paths/62-unique-paths.java
@@ -1,42 +1,14 @@
 class Solution {
     public int uniquePaths(int m, int n) {
         int[][] arr = new int[m + 1][n + 1];
-        arr[1][1] = 1;
-        Queue<Point> q = new LinkedList<>();
-        q.add(new Point(1, 1));
+        arr[0][1] = 1;
         
-        boolean[][] v = new boolean[m + 1][n + 1];
-        
-        while(!q.isEmpty()) {
-            int size = q.size();
-            
-            for(int i = 0; i < size; i++) {
-                Point now = q.poll();
-                
-                if(v[now.y][now.x]) continue;
-                
-                if(now.x + 1 <= n && now.y <= m) {
-                    arr[now.y][now.x + 1] += arr[now.y][now.x];
-                    q.add(new Point(now.x + 1, now.y));
-                }
-                
-                if(now.x <= n && now.y + 1 <= m) {
-                    arr[now.y + 1][now.x] += arr[now.y][now.x];
-                    q.add(new Point(now.x, now.y + 1));
-                }
-                
-                v[now.y][now.x] = true;
+        for(int i = 1; i <= m; i++) {
+            for(int j = 1; j <= n; j++) {
+                arr[i][j] = arr[i - 1][j] + arr[i][j - 1];
             }
         }
         
         return arr[m][n];
-    }
-    
-    class Point{
-        int x, y;
-        public Point(int x, int y) {
-            this.x = x;
-            this.y = y;
-        }
     }
 }
