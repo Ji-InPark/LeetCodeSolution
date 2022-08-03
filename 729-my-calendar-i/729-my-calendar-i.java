b0a7821c54d0f1d8d193f124fdfd6e7a536ea4f6
Ji-In Park <81283634+Ji-InPark@users.noreply.github.com>
2022-08-03 20:12:58 +0900

    Time: 66 ms (29.00%), Space: 54.2 MB (75.36%) - LeetHub

---
+++ 729-my-calendar-i/729-my-calendar-i.java
@@ -0,0 +1,28 @@
+class MyCalendar {
+    TreeMap<Integer, Integer> map = new TreeMap<>();
+    
+    public MyCalendar() {
+        
+    }
+    
+    public boolean book(int start, int end) {
+        int s = -1, e = -1;
+        if(map.floorKey(start) != null) {
+            s = map.floorKey(start);
+        }
+        
+        if(map.floorKey(--end) != null) {
+            e = map.floorKey(end);
+        }
+        
+        if(s - e != 0) return false;
+        
+        if(s != -1 && start <= map.get(s)) return false;
+        
+        if(e != -1 && end <= map.get(e)) return false;
+        
+        map.put(start, end);
+        
+        return true;
+    }
+}
