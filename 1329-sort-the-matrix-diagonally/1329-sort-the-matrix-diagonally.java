247ee9a6faa8189faa500d9a9261704dbbde35d4
Ji-In Park <81283634+Ji-InPark@users.noreply.github.com>
2022-08-28 13:16:39 +0900

    Time: 31 ms (5.03%), Space: 50.2 MB (17.10%) - LeetHub

---
+++ 1329-sort-the-matrix-diagonally/1329-sort-the-matrix-diagonally.java
@@ -0,0 +1,64 @@
+class Solution {
+    public int[][] diagonalSort(int[][] mat) {
+        PriorityQueue<Point> pq = new PriorityQueue<>();
+        Queue<Point> q = new LinkedList<>();
+        int r = mat[0].length, c = mat.length;
+        pq.add(new Point(r - 1, 0, mat[0][r - 1]));
+        q.add(new Point(r - 1, 0, mat[0][r - 1]));
+        
+        int[][] result = new int[c][r];
+        
+        while(!q.isEmpty()) {
+            int size = q.size();
+            
+            for(int i = 0; i < size - 1; i++) {
+                Point qnow = q.poll(), pqnow = pq.poll();
+                
+                //System.out.print(pqnow.value + " ");
+                
+                result[qnow.y][qnow.x] = pqnow.value;
+                
+                if(qnow.x - 1 >= 0) {
+                    q.add(new Point(qnow.x - 1, qnow.y, mat[qnow.y][qnow.x - 1]));
+                }
+            }
+            
+            Point qnow = q.poll(), pqnow = pq.poll();
+            
+            //System.out.println(pqnow.value + " ");
+                
+            result[qnow.y][qnow.x] = pqnow.value;
+
+            if(qnow.x - 1 >= 0) {
+                q.add(new Point(qnow.x - 1, qnow.y, mat[qnow.y][qnow.x - 1]));
+            }
+            
+            if(qnow.y + 1 < c) {
+                q.add(new Point(qnow.x, qnow.y + 1, mat[qnow.y + 1][qnow.x]));
+            }
+            
+            size = q.size();
+            for(int i = 0; i < size; i++){
+                Point p = q.poll();
+                pq.add(p);
+                q.add(p);
+            }
+        }
+        
+        return result;
+    }
+    
+    class Point implements Comparable<Point>{
+        int x, y, value;
+        public Point(int x, int y, int value) {
+            this.x = x;
+            this.y = y;
+            this.value = value;
+        }
+        
+        @Override
+        public int compareTo(Point p) {
+            return this.value - p.value;
+        }
+    }
+}
