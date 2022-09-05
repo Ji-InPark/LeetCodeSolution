a1f79ff4a04de8a96988ee6b2c90e2458f4ca514
Ji-In Park <81283634+Ji-InPark@users.noreply.github.com>
2022-09-05 17:49:48 +0900

    Time: 4 ms (64.45%), Space: 47 MB (74.58%) - LeetHub

---
+++ 429-n-ary-tree-level-order-traversal/429-n-ary-tree-level-order-traversal.java
@@ -0,0 +1,34 @@
+class Solution {
+    public List<List<Integer>> levelOrder(Node root) {
+        if(root == null) {
+            return new ArrayList<>();
+        }
+        
+        Queue<Node> q = new LinkedList<>();
+        q.add(root);
+        
+        List<List<Integer>> result = new ArrayList<>();
+        
+        while(!q.isEmpty()) {
+            int size = q.size();
+            
+            List<Integer> temp = new ArrayList<>();
+            
+            for(int i = 0; i < size; i++) {
+                Node now = q.poll();
+                
+                temp.add(now.val);
+                
+                List<Node> list = now.children;
+                
+                for(int j = 0; j < list.size(); j++) {
+                    q.add(list.get(j));
+                }
+            }
+            
+            result.add(temp);
+        }
+        
+        return result;
+    }
+}
