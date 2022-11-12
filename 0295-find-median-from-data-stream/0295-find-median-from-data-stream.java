556c37173689a1e8589818b7b484d7924a28810e
Ji-In Park <81283634+Ji-InPark@users.noreply.github.com>
2022-11-12 20:55:05 +0900

    Time: 376 ms (8.00%), Space: 108.6 MB (80.16%) - LeetHub

---
+++ 0295-find-median-from-data-stream/0295-find-median-from-data-stream.java
@@ -0,0 +1,51 @@
+class MedianFinder {
+    PriorityQueue<Integer> spq = new PriorityQueue<>((a, b) -> b - a);
+    PriorityQueue<Integer> bpq = new PriorityQueue<>();
+
+    public MedianFinder() {
+        
+    }
+    
+    public void addNum(int num) {
+        var small = spq.peek();
+        var big = bpq.peek();
+        
+        if(small == null && big == null) {
+            spq.add(num);
+        } else if(small == null) {
+            if(big <= num) {
+                spq.add(bpq.poll());
+                bpq.add(num);
+            } else {
+                spq.add(num);
+            }
+        } else {
+            if(small >= num) {
+                bpq.add(spq.poll());
+                spq.add(num);
+            } else {
+                bpq.add(num);
+            }
+        }
+        
+        while(Math.abs(spq.size() - bpq.size()) > 1) {
+            if (spq.size() > bpq.size()) bpq.add(spq.poll());
+            else spq.add(bpq.poll());
+        }
+    }
+    
+    public double findMedian() {
+        var small = spq.peek();
+        var big = bpq.peek();
+        
+        if(spq.size() != bpq.size()) {
+            if(spq.size() > bpq.size()) return small;
+            else return big;
+        }
+        
+        if(small == null) small = 0;
+        if(big == null) big = 0;
+        
+        return (double)(small + big) / 2;
+    }
+}
