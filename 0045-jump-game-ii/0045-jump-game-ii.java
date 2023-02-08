063a4366406c6f2d26401b017d7ccd2ffe3628d5
Ji-In Park <81283634+Ji-InPark@users.noreply.github.com>
2023-02-08 18:16:02 +0900

    Time: 53 ms (30.67%), Space: 42.7 MB (64.47%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.java
+++ 0045-jump-game-ii/0045-jump-game-ii.java
@@ -2,12 +2,10 @@ class Solution {
     public int jump(int[] nums) {
         int n = nums.length;
         var arr = new int[n];
+        
         if(n == 1) return 0;
-        if(nums[0] >= n - 1) return 1;
         
-        for(int i = 0; i <= nums[0]; i++) arr[i] = 1;
-        
-        for(int i = 1; i < n; i++) {
+        for(int i = 0; i < n; i++) {
             if(i + nums[i] >= n - 1) return arr[i] + 1;
             
             for(int j = i + 1; j <= i + nums[i]; j++) {
