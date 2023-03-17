a1794a3310954776370ce81d189b7185e6f45577
Ji-In Park <81283634+Ji-InPark@users.noreply.github.com>
2023-03-17 10:13:20 +0900

    Time: 47 ms (32.26%), Space: 57.6 MB (18.89%) - LeetHub

---
+++ 0208-implement-trie-prefix-tree/0208-implement-trie-prefix-tree.java
@@ -0,0 +1,33 @@
+class Trie {
+    Trie[] tries = new Trie[26];
+    boolean hasValue = false, isEnd = false;
+
+    public void insert(String word) {
+        if(word.isEmpty()) {
+            isEnd = true;
+            return;
+        }
+        
+        int index = word.charAt(0) - 'a';
+        
+        if(tries[index] == null) tries[index] = new Trie();
+        tries[index].insert(word.substring(1));
+        hasValue = true;
+    }
+    
+    public boolean search(String word) {
+        if(word.isEmpty()) return !hasValue || isEnd;
+        
+        int index = word.charAt(0) - 'a';
+        
+        return tries[index] != null && tries[index].search(word.substring(1));
+    }
+    
+    public boolean startsWith(String prefix) {
+        if(prefix.isEmpty()) return true;
+        
+        int index = prefix.charAt(0) - 'a';
+        
+        return tries[index] != null && tries[index].startsWith(prefix.substring(1));
+    }
+}
