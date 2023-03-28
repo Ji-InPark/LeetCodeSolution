0b86413936b33488cdd3dc074b0384e55fffaf91
Ji-In Park <81283634+Ji-InPark@users.noreply.github.com>
2023-03-28 15:27:46 +0900

    Time: 3 ms (21.01%), Space: 42.1 MB (16.27%) - LeetHub

--- 0983-minimum-cost-for-tickets/0983-minimum-cost-for-tickets.java
+++ 0983-minimum-cost-for-tickets/0983-minimum-cost-for-tickets.java
@@ -2,8 +2,10 @@ class Solution {
     public int mincostTickets(int[] days, int[] costs) {
         var set = new HashSet<Integer>();
         for(var day: days) set.add(day);
-        int n = days.length;
+        
         var ranges = new int[] {1, 7, 30};
+        
+        int n = days.length;
         var dp = new int[days[n - 1] + 1];
         Arrays.fill(dp, Integer.MAX_VALUE);
         dp[0] = 0;
@@ -11,13 +13,13 @@ class Solution {
         int pre = 0;
         
         for(int day = 0; day < dp.length; day++) {
+            if(!set.contains(day)) {
+                dp[day] = dp[pre];
+                continue;
+            }
+            
             for(int j = 0; j < 3; j++) {
                 int beforeDay = Math.max(day - ranges[j], 0), cost = costs[j];
-                
-                if(!set.contains(day)) {
-                    dp[day] = dp[pre];
-                    continue;
-                }
                 
                 pre = day;
                 
