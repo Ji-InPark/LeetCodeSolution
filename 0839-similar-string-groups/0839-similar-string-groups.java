22c415dea8496871bf6ec771a198c83b3a024ce1
Ji-In Park <81283634+Ji-InPark@users.noreply.github.com>
2023-04-28 13:01:50 +0900

    Time: 64 ms (45.04%), Space: 43 MB (5.73%) - LeetHub

--- 0839-similar-string-groups/0839-similar-string-groups.java
+++ 0839-similar-string-groups/0839-similar-string-groups.java
@@ -7,7 +7,6 @@ class Solution {
         
         for(var str: strs) {
             if(set.contains(str)) continue;
-            set.add(str);
             solve(str);
             result++;
         }
@@ -16,21 +15,20 @@ class Solution {
     }
     
     private void solve(String str1) {
+        set.add(str1);
+        var arr1 = str1.toCharArray();
+        
         for(var str2: strs) {
             if(set.contains(str2)) continue;
             
-            var arr1 = str1.toCharArray();
             var arr2 = str2.toCharArray();
             
             int diff = 0;
-            for(int i = 0; i < arr1.length && diff <= 2; i++) {
+            for(int i = 0; i < arr1.length && diff <= 2; i++)
                 if(arr1[i] != arr2[i]) diff++;
-            }
+
             
-            if(diff == 2) {
-                set.add(str2);
-                solve(str2);
-            }
+            if(diff == 2) solve(str2);
         }
     }
 }
