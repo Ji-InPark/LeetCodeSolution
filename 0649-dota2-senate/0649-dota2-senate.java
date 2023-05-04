51fccf275398747ad7f9b0edfc35f20b1738af25
Ji-In Park <81283634+Ji-InPark@users.noreply.github.com>
2023-05-04 18:16:03 +0900

    Time: 10 ms (73.86%), Space: 43.4 MB (22.16%) - LeetHub

--- 0649-dota2-senate/0649-dota2-senate.java
+++ 0649-dota2-senate/0649-dota2-senate.java
@@ -1,31 +1,28 @@
 class Solution {
     public String predictPartyVictory(String senate) {
-        var dq = new ArrayDeque<Character>();
-        for(var c: senate.toCharArray()) dq.add(c);
+        var dq = new LinkedList<Boolean>();
+        for(var c: senate.toCharArray()) dq.add(c == 'D');
 
-        int d = 0, r = 0, flag = 3;
+        int[] dr = new int[]{0, 0, 0};
+        int flag = 3;
         while(flag == 3) {
             flag = 0;
             int size = dq.size();
             
             for(int i = 0; i < size; i++) {
-                if(dq.pollFirst() == 'D') {
-                    flag |= 1;
-                    if(r > 0) r--;
-                    else {
-                        d++;
-                        dq.add('D');
-                    }
-                } else {
-                    flag |= 2;
-                    if(d > 0) d--;
-                    else {
-                        r++;
-                        dq.add('R');
-                    }
+                var now = dq.poll();
+                int i1 = now ? 1 : 2;
+                int i2 = now ? 2 : 1;
+                
+                if(dr[i2] > 0) {
+                    dr[i2]--;
+                    continue;
                 }
+                
+                flag |= i1;
+                dr[i1]++;
+                dq.add(now);
             }
-            
         }
         
         return flag == 1 ? "Dire" : "Radiant";
