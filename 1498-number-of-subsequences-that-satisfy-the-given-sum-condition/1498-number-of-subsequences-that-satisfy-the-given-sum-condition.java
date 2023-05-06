025ccac9a8e85131c4b6fb18ed82bac0fc688334
Ji-In Park <81283634+Ji-InPark@users.noreply.github.com>
2023-05-07 01:04:03 +0900

    Time: 33 ms (100.00%), Space: 50.7 MB (88.00%) - LeetHub

--- 1498-number-of-subsequences-that-satisfy-the-given-sum-condition/1498-number-of-subsequences-that-satisfy-the-given-sum-condition.java
+++ 1498-number-of-subsequences-that-satisfy-the-given-sum-condition/1498-number-of-subsequences-that-satisfy-the-given-sum-condition.java
@@ -6,13 +6,12 @@ class Solution {
         pows[0] = 1;
         for (int i = 1 ; i < n ; ++i)
             pows[i] = pows[i - 1] * 2 % mod;
+         
         while (l <= r) {
-            if (A[l] + A[r] > target) {
-                r--;
-            } else {
-                res = (res + pows[r - l++]) % mod;
-            }
+            if (A[l] + A[r] > target) r--;
+            else res = (res + pows[r - l++]) % mod;
         }
         return res;
     }
 }
+
