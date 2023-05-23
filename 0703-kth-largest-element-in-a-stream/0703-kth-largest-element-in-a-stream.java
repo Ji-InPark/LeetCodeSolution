9d5f4fb1d2fe1fbab321c989d410dc453e2c778f
Ji-In Park <81283634+Ji-InPark@users.noreply.github.com>
2023-05-23 12:14:01 +0900

    Time: 17 ms (53.19%), Space: 49 MB (7.80%) - LeetHub

--- 0703-kth-largest-element-in-a-stream/0703-kth-largest-element-in-a-stream.java
+++ 0703-kth-largest-element-in-a-stream/0703-kth-largest-element-in-a-stream.java
@@ -1,16 +1,15 @@
 class KthLargest {
     PriorityQueue<Integer> pq = new PriorityQueue<>();
-    int k;
     
     public KthLargest(int k, int[] nums) {
-        this.k = k;
+        for(int i = 0; i < k - nums.length; i++) pq.add(-1000000);
         for(var num: nums) pq.add(num);
         while(pq.size() > k) pq.poll();
     }
     
     public int add(int val) {
         pq.add(val);
-        while(pq.size() > k) pq.poll();
+        pq.poll();
         return pq.peek();
     }
 }
