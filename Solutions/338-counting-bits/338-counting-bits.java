c63c64e7dec2920e5bac9a5cf79752cbc2202793
Ji-In Park <81283634+Ji-InPark@users.noreply.github.com>
2023-06-01 22:30:23 +0900

    Delete Solutions directory

--- Solutions/338-counting-bits/338-counting-bits.java
+++
@@ -1,8 +0,0 @@
-class Solution {
-    public int[] countBits(int n) {
-        int[] result = new int[n + 1];
-        for(int i = 0; i <= n; i++)
-            result[i] = result[i >> 1] + (i & 1);   
-        return result;
-    }
-}
--- Solutions/81-search-in-rotated-sorted-array-ii/81-search-in-rotated-sorted-array-ii.java
+++
@@ -1,5 +0,0 @@
-class Solution {
-    public boolean search(int[] nums, int target) {
-        for(int num : nums) if(num == target) return true; return false;
-    }
-}
