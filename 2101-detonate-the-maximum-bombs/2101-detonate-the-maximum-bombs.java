70ec6c988d3fc5811a74ab23074a5c06bec460ec
Ji-In Park <81283634+Ji-InPark@users.noreply.github.com>
2023-06-02 20:26:42 +0900

    Time: 132 ms (47.15%), Space: 44.1 MB (9.80%) - LeetHub

---
+++ 2101-detonate-the-maximum-bombs/2101-detonate-the-maximum-bombs.java
@@ -0,0 +1,35 @@
+class Solution {
+    public int maximumDetonation(int[][] bombs) {
+        var set = new HashSet<Integer>();
+        int result = 0;
+        
+        for(int i = 0; i < bombs.length; i++) {
+            if(set.contains(i)) continue;
+            
+            var dfsSet = dfs(bombs, new HashSet<>(), i);
+            
+            result = Math.max(result, dfsSet.size());
+            
+            for(var num : dfsSet) set.add(num);
+        }
+        
+        return result;
+    }
+    
+    private HashSet<Integer> dfs(int[][] bombs, HashSet<Integer> set, int now) {
+        set.add(now);
+        
+        for(int i = 0; i < bombs.length; i++) {
+            if(set.contains(i)) continue;
+            
+            var a = bombs[now];
+            var b = bombs[i];
+            
+            long length = a[2], x = Math.abs(a[0] - b[0]), y = Math.abs(a[1] - b[1]);
+            
+            if(length * length >= x * x + y * y) dfs(bombs, set, i);
+        }
+        
+        return set;
+    }
+}
