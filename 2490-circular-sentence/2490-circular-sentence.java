a0f0d26a21b6cac0de0c5ab360d4f933d6435f46
Ji-In Park <81283634+Ji-InPark@users.noreply.github.com>
2023-06-29 15:04:32 +0900

    Time: 1 ms (98.69%), Space: 40.8 MB (94.78%) - LeetHub

--- 2490-circular-sentence/2490-circular-sentence.java
+++ 2490-circular-sentence/2490-circular-sentence.java
@@ -2,18 +2,12 @@ class Solution {
     public boolean isCircularSentence(String sentence) {
         var arr = sentence.split(" ");
         
-        for(int i = 1; i < arr.length; i++) {
-            if(last(arr[i - 1]) != first(arr[i])) return false;
-        }
+        for(int i = 1; i < arr.length; i++) if(last(arr[i - 1]) != first(arr[i])) return false;
         
         return first(arr[0]) == last(arr[arr.length - 1]);
     }
     
-    private char first(String s) {
-        return s.charAt(0);
-    }
+    private char first(String s) {return s.charAt(0);}
     
-    private char last(String s) {
-        return s.charAt(s.length() - 1);
-    }
+    private char last(String s) {return s.charAt(s.length() - 1);}
 }
