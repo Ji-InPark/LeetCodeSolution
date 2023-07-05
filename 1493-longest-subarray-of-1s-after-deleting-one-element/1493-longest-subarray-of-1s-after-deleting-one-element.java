a095410e8bc9991194574766f109393664053132
Ji-In Park <81283634+Ji-InPark@users.noreply.github.com>
2023-07-05 18:00:15 +0900

    Time: 4 ms (7.54%), Space: 54.3 MB (7.16%) - LeetHub

--- 1493-longest-subarray-of-1s-after-deleting-one-element/1493-longest-subarray-of-1s-after-deleting-one-element.java
+++ 1493-longest-subarray-of-1s-after-deleting-one-element/1493-longest-subarray-of-1s-after-deleting-one-element.java
@@ -1,24 +1,14 @@
 class Solution {
     public int longestSubarray(int[] nums) {
-        int n = nums.length;
+        int n = nums.length, result = 0;
         var lArr = new int[n];
         var rArr = new int[n];
         
         for(int i = 1; i < n; i++) {
-            if(nums[i - 1] == 1) {
-                lArr[i] = lArr[i - 1] + 1;
-            } else {
-                lArr[i] = 0;
-            }
-            
-            if(nums[n - i] == 1) {
-                rArr[n - i - 1] = rArr[n - i] + 1;
-            } else {
-                rArr[n - i - 1] = 0;
-            }
+            lArr[i] = (lArr[i - 1] + 1) * nums[i - 1];
+            rArr[n - i - 1] = (rArr[n - i] + 1) * nums[n - i];
         }
         
-        int result = 0;
         for(int i = 0; i < n; i++) result = Math.max(result, lArr[i] + rArr[i]);
         
         return result;
